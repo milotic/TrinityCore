3d1196f56f55b93bae0c33cb3a83a85e8f9384c6
Milotic <al0305@hotmail.com>
2013-10-09 00:26:45 -0600

    Templates IoC

---
+++ sql/updates/world/2013_10_09_00_world_isle_of_conquest_templates.sql
@@ -0,0 +1,13 @@
+-- Isle of conquest level 80
+
+UPDATE creature_template SET difficulty_entry_1 = 35415 WHERE entry = 34775; -- Demolisher
+UPDATE creature_template SET difficulty_entry_1 = 35433 WHERE entry = 35069; -- Horde Siege Engine
+UPDATE creature_template SET difficulty_entry_1 = 35431 WHERE entry = 34776; -- Alliance Siege Engine
+UPDATE creature_template SET difficulty_entry_1 = 35413 WHERE entry = 34793; -- Catapult
+UPDATE creature_template SET difficulty_entry_1 = 35421 WHERE entry = 35273; -- Horde Glaive Thrower
+UPDATE creature_template SET difficulty_entry_1 = 35419 WHERE entry = 34802; -- Alliance Glaive Thrower
+UPDATE creature_template SET difficulty_entry_1 = 35429 WHERE entry = 34944; -- Keep Cannon
+UPDATE creature_template SET difficulty_entry_1 = 35401 WHERE entry = 34919; -- 7th Legion Infantry
+UPDATE creature_template SET difficulty_entry_1 = 35403 WHERE entry = 34924; -- High Commander Halford Wyrmbane <7th Legion>
+UPDATE creature_template SET difficulty_entry_1 = 35407 WHERE entry = 34918; -- Kor'kron Guard
+UPDATE creature_template SET difficulty_entry_1 = 35405 WHERE entry = 34922; -- Overlord Agmar
